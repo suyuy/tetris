9d308a9daecc46cbc1f9a3ee88c9928358f08e98
suyuy <81351506+suyuy@users.noreply.github.com>
2025-02-22 00:34:06 +0900

    Update game.js

--- game.js
+++ game.js
@@ -10,29 +10,6 @@ let board = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
 let currentPiece = getRandomPiece();
 let x = 3, y = 0;
 let gameOver = false;
-let gameInterval = setInterval(moveDown, 500);  // ゲームループを管理
-
-function moveDown() {
-  if (gameOver) return; // ゲームオーバーなら何もしない
-
-  if (canMove(x, y + 1, currentPiece.shape)) {
-    y++;
-  } else {
-    mergePiece();
-    clearLines();
-    currentPiece = getRandomPiece();
-    x = 3;
-    y = 0;
-
-    if (!canMove(x, y, currentPiece.shape)) {
-      gameOver = true;
-      clearInterval(gameInterval); // ゲームループを停止
-      alert("ゲームオーバー！");
-      return;
-    }
-  }
-  drawBoard(board, currentPiece, x, y);
-}
 
 function getRandomPiece() {
   let id = Math.floor(Math.random() * SHAPES.length);
